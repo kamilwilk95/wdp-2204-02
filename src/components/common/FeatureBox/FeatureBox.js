3f39736576d25caf32cbcbd2fdbc5e50422140ab
Vladyslav Sydorenko <cayceworld@outlook.com>
2022-04-16 16:34:40 +0200

    Add link

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { Link } from 'react-router-dom';
 import styles from './FeatureBox.module.scss';
 
 const FeatureBox = ({ active, icon, children }) => (
-  <div className={styles.root + (active ? ' ' + styles.active : '')}>
-    {icon && (
-      <div className={styles.iconWrapper}>
-        <FontAwesomeIcon className={styles.icon} icon={icon} />
-      </div>
-    )}
-    <div className={styles.content}>{children}</div>
-  </div>
+  <Link>
+    <div className={styles.root + (active ? ' ' + styles.active : '')}>
+      {icon && (
+        <div className={styles.iconWrapper}>
+          <FontAwesomeIcon className={styles.icon} icon={icon} />
+        </div>
+      )}
+      <div className={styles.content}>{children}</div>
+    </div>
+  </Link>
 );
 
 FeatureBox.propTypes = {
