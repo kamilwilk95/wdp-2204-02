abe42272c1d863105deba7586ffdbd53d6f647a3
Beata Karwowska <bsz3636@o2.pl>
2022-04-28 20:40:06 +0200

    add changes

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import styles from './Brands.module.scss';
+import clsx from 'clsx';
 
+import styles from './Brands.module.scss';
 
 import { useSelector } from 'react-redux';
 import { getAllBrands } from '../../../redux/brandRedux';
@@ -10,25 +11,40 @@ import { getAllBrands } from '../../../redux/brandRedux';
 const Brands = () => {
 
   const brands = useSelector(state => getAllBrands(state));
-  console.log('brands', brands);
+  //console.log('brands', brands);
 
-  
+
+
+
+  const [slideIndex] = useState(0);
 
   return (
     <div className={styles.root}>
       <div className='container'>
-        <div className='row'>
+        <div>
+          <div className={clsx('row', styles.brandsWrapper)}>
 
-          <div className='col-1'>
-Hello
-          </div>
+            <div className='col-1'>
 
-          <div className='col-10'>
-            <div className='row'>
+            </div>
+
+            <div className='col-10'>
+              <div className='row'>
+                {brands
+                  .slice(slideIndex * 6, (slideIndex + 1) * 6)
+                  .map(brand => (
+                    <div className={clsx('col-2', styles.imgWrapper)} key={brand.id}>
+                      <img className={styles.brandImg}
+                        alt={brand.name}
+                        src={brand.image}
+                      />
+                    </div>
+                  ))}
 
 
 
 
+              </div>
             </div>
           </div>
 
