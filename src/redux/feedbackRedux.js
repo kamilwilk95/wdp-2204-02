a35383af314a65338c4795ba2484c46774c3a625
Mariusz Banaś <mb0986@wp.pl>
2022-05-01 12:02:04 +0200

    Add Swipable

--- src/redux/feedbackRedux.js
+++ src/redux/feedbackRedux.js
@@ -1,6 +1,9 @@
 /* selectors */
 export const getAllFeedback = ({ feedback }) => feedback;
 
+export const getFeedbackById = ({ feedback }, feedbackId) =>
+  feedback.find(item => item.id === feedbackId);
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
