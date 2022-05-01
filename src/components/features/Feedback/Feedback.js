506c09ee31d4530f13b42b1f9bc0d307f3acbdf7
Mariusz Banaś <mb0986@wp.pl>
2022-05-01 12:28:31 +0200

    Add Feedback

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -1,61 +1,117 @@
 import React from 'react';
-import styles from './Feedback.module.scss';
+import PropTypes from 'prop-types';
 
-import { useSelector } from 'react-redux';
-import { getAllFeedback } from '../../../redux/feedbackRedux';
+import styles from './Feedback.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 
-const Feedback = () => {
-  const feedback = useSelector(getAllFeedback);
+import Swipable from '../../common/Swipeable/Swipable';
 
-  return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className={styles.panelBar}>
-          <div className='row no-gutters align-items-end'>
-            <div className={'col-md-auto col-sm-12 col-12 ' + styles.heading}>
-              <h3>CLIENT FEEDBACK</h3>
+// import clsx from 'clsx';
+
+class Feedback extends React.Component {
+  state = {
+    activePage: 1,
+  };
+
+  handlePageChange(newPage) {
+    this.setState({ activePage: newPage });
+  }
+
+  render() {
+    const { feedback } = this.props;
+    const { activePage } = this.state;
+
+    const singleFeedback = feedback.find(item => item.id === activePage);
+    const pagesCount = feedback.length;
+
+    const dots = [];
+    for (let i = 1; i < pagesCount + 1; i++) {
+      dots.push(
+        <li>
+          <a
+            onClick={() => this.handlePageChange(i)}
+            className={i === activePage && styles.active}
+          >
+            page {i}
+          </a>
+        </li>
+      );
+    }
+
+    const leftAction = () => {
+      this.setState({ activePage: activePage + 1 });
+
+      if (activePage >= pagesCount) {
+        this.setState({ activePage: activePage + 0 });
+      }
+    };
+
+    const rightAction = () => {
+      this.setState({ activePage: activePage - 1 });
+
+      if (activePage === 1) {
+        this.setState({ activePage: activePage + 0 });
+      }
+    };
+
+    return (
+      <Swipable leftAction={leftAction} rightAction={rightAction}>
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={styles.panelBar}>
+              <div className='row no-gutters align-items-end'>
+                <div className={'col-md-auto col-sm-12 col-12 ' + styles.heading}>
+                  <h3>CLIENT FEEDBACK</h3>
+                </div>
+                <div className={'col-md-auto col-sm-auto col-auto ' + styles.dots}>
+                  <ul>{dots}</ul>
+                </div>
+              </div>
             </div>
-            <div className={'col-md-auto col-sm-auto col-auto ' + styles.dots}>
-              <ul>
-                <li>
-                  <a className={styles.active} href='#'></a>
-                </li>
-                <li>
-                  <a href='#'></a>
-                </li>
-                <li>
-                  <a href='#'></a>
-                </li>
-              </ul>
+            <div className={styles.content}>
+              <FontAwesomeIcon
+                className={styles.icon}
+                icon={faQuoteRight}
+              ></FontAwesomeIcon>
+              <div className={styles.comment}>{singleFeedback.comment}</div>
+              <div className={styles.signature}>
+                <div className={styles.photoBox}>
+                  <img
+                    className={styles.photo}
+                    alt={singleFeedback.name}
+                    src={`${process.env.PUBLIC_URL}${singleFeedback.photo}`}
+                  />
+                </div>
+                <div className={styles.description}>
+                  <span>{singleFeedback.name}</span>
+                  <br />
+                  {singleFeedback.description}
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className={styles.content}>
-          <FontAwesomeIcon
-            className={styles.icon}
-            icon={faQuoteRight}
-          ></FontAwesomeIcon>
-          <div className={styles.comment}>{feedback[0].comment}</div>
-          <div className={styles.signature}>
-            <div className={styles.photoBox}>
-              <img
-                className={styles.photo}
-                alt={feedback[0].name}
-                src={`${process.env.PUBLIC_URL}${feedback[0].photo}`}
-              />
-            </div>
-            <div className={styles.description}>
-              <span>{feedback[0].name}</span>
-              <br />
-              {feedback[0].description}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+      </Swipable>
+    );
+  }
+}
+
+Feedback.propTypes = {
+  children: PropTypes.node,
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      comment: PropTypes.string,
+      name: PropTypes.string,
+      photo: PropTypes.string,
+      description: PropTypes.string,
+    })
+  ),
+};
+
+Feedback.defaultProps = {
+  feedback: [],
 };
 
 export default Feedback;
